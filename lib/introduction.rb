14128512a35d621d5915d2a442579105069dd0b4
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-16 16:45:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,8 @@ def introduction(name)
   "Hi, my name is #{name}.\n"
 end
 
-def
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
 introduction("Josh")
+introduction_with_language("Dan", "language")
