cf992a451598dcb6551599a9bf006c85a641c0b2
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-16 16:46:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,4 @@ def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 introduction("Josh")
-introduction_with_language("Dan", "language")
+introduction_with_language("Dan", "Ember.js")
